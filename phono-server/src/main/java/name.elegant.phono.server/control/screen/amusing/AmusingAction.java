c0fd5c97e0c08919665ea7a975a516c86a1970b3
GarryKing <hezeqing001@gmail.com>
2013-04-01 21:38:05 +0800

    日志读取 重构

---
+++ phono-server/src/main/java/name.elegant.phono.server/control/screen/amusing/AmusingAction.java
@@ -0,0 +1,41 @@
+package name.elegant.phono.server.control.screen.amusing;
+
+import name.elegant.phono.core.admin.dao.IpAddressDAO;
+import name.elegant.phono.core.util.net.NetConnection;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Author: Garry King
+ * Date: 13-2-11 上午1:36
+ * E-mail:flyhzq@sina.com
+ */
+@Controller
+@RequestMapping("/amusing/index.html")
+public class AmusingAction {
+
+    @Resource
+    private IpAddressDAO ipAddressDAO;
+
+    @RequestMapping
+    public ModelAndView testDoIt() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+                .getRequestAttributes()).getRequest();
+        String currIp = request.getRemoteAddr();
+        String address = ipAddressDAO.queryAddressByIp(currIp);
+        if (address == null || address.equals("")) {
+            String query = NetConnection.getURLContent("http://www.cha086.com/app/ip/" + currIp).split("class=\"last\"")[1];
+            address = NetConnection.getSubStringWithTag(query, "：", "</td>");
+            ipAddressDAO.insertNewIpAddress(currIp, address);
+        }
+        String result = "您好，来自 " + address + " 的朋友，您的ip是：" + currIp;
+        return new ModelAndView("user/logined", "result", result);
+    }
+
+}
---
+++ phono-server/src/main/java/name/elegant/phono/server/vo/amusing/IpLogVO.java
@@ -0,0 +1,9 @@
+package name.elegant.phono.server.vo.amusing;
+
+/**
+ * User: zeqing.hzq
+ * Date: 13-4-1
+ * Time: обнГ5:40
+ */
+public class IpLogVO {
+}
