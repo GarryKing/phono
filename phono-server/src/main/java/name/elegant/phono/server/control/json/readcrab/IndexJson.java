d4ba9e553e6b941ef6e0fe955d97af638b28c902
Garry <hezeqing001@gmail.com>
2013-04-22 23:30:05 +0800

    json

--- phono-server/src/main/java/name/elegant/phono/server/control/json/readcrab/IndexJson.java
+++ phono-server/src/main/java/name/elegant/phono/server/control/json/readcrab/IndexJson.java
@@ -1,31 +1,38 @@
-package name.elegant.phono.server.control.screen;
+package name.elegant.phono.server.control.json.readcrab;
 
 import name.elegant.phono.client.dataobject.redcrab.PictureDO;
 import name.elegant.phono.core.redcrab.dao.PictureDAO;
+import name.elegant.phono.core.util.net.NetConnection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.List;
 
 /**
- * User: Administrator
- * Date: 13-4-17
- * Time: ÏÂÎç11:51
+ * Author: Garry King
+ * Date: 13-2-11 ÉÏÎç1:36
+ * E-mail:flyhzq@sina.com
  */
 @Controller
-@RequestMapping("/index.html")
-public class IndexAction {
+@RequestMapping("/json")
+public class IndexJson {
 
     @Autowired
     private PictureDAO pictureDAO;
 
-    @RequestMapping
-    public ModelAndView showPicture(HttpServletRequest request, HttpServletResponse response) {
+    @RequestMapping(value = "/indexImage.crab", method = RequestMethod.GET)
+    @ResponseBody
+    public Object testDoIt(HttpServletResponse response) throws IOException {
         List<PictureDO> picList = pictureDAO.queryAllPictureDO();
-        return new ModelAndView("index", "picList", picList);
+        return picList;
     }
+
 }
