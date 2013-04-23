b2c877c3dcaef8860d1856e82bf18d9e1f8dae76
GarryKing <hezeqing001@gmail.com>
2013-04-24 00:24:47 +0800

    首页图片展示

--- phono-server/src/main/webapp/js/redcrab/redcrab.js
+++ phono-server/src/main/webapp/js/redcrab/redcrab.js
@@ -13,7 +13,9 @@ function initGlobalParameter() {
 }
 
 function initContent() {
-    clientVisibleWidth
+    $("#content").ajaxStop(function(){
+        $("#content").append("Ω· ¯¡À")
+    });
 }
 
 function loadImages() {
@@ -24,8 +26,8 @@ function loadImages() {
             for (var i = 0; i < data.length; i++) {
                 var str = createImageDiv(i, data[i]);
                 $("#content").append(str);
+                setImageCss(i);
             }
-
         },
         error: function () {
 
@@ -34,9 +36,18 @@ function loadImages() {
 
     function createImageDiv(order, data) {
         window.sc = "<img src=" + data.sourceUrl + "?" + Math.random() + ">";
-        return "<div id='image_'" + order + " class='image_wrapper'>"
-            + "<iframe  src='javascript:parent.sc'></iframe>"
+        return "<div id='image_" + order + "' class='image_wrapper'>"
+            + "<iframe scrolling='no' src='javascript:parent.sc'></iframe>"
             + "</div>";
+    }
+
+    function setImageCss(order){
+        var currIframe = document.getElementById("image_"+order).getElementsByTagName("iframe")[0];
+        var currDocument = currIframe.contentWindow.document;
+        currIframe.onload= "javascript:var x=document.getElementById('image_"+order+"').getElementsByTagName('iframe')[0].contentWindow.document.images[0];this.width=x.width;this.height=x.height;";
+        currDocument.body.style.margin="0px";
+        currDocument.images[0].width="214";
+//        currIframe.height=currDocument.images[0].height;
     }
 }
 
